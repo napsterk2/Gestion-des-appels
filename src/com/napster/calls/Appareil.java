26ec9646ae8fe856a58c01c1a65320294cd34d89
napster <napsterpaulin@gmail.com>
2019-08-28 01:59:13 +0200

    implementation des methodes de l'interface dans la classe appareil

--- src/com/napster/calls/Appareil.java
+++ src/com/napster/calls/Appareil.java
@@ -1,4 +1,43 @@
 package com.napster.calls;
 
-public class Appareil {
+import java.util.Date;
+import java.util.List;
+
+public class Appareil implements Iappareil {
+
+
+    @Override
+    public void enregistrer(Contacts c) {
+
+    }
+
+    @Override
+    public void enregistrer(Appel a, String numtel) {
+
+    }
+
+    @Override
+    public Contacts consulter(int numero) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Contacts> consulter(String mc) {
+        return null;
+    }
+
+    @Override
+    public double coutAppels(Date d1, Date d2) {
+        return 0;
+    }
+
+    @Override
+    public double coutTotalAppels() {
+        return 0;
+    }
+
+    @Override
+    public double coutAppels(int numero) {
+        return 0;
+    }
 }
