a30d648d5799bc1a4a61f19e9f06af4f87cbd4c9
napster <napsterpaulin@gmail.com>
2019-08-27 20:46:05 +0200

    initialisation du projet creation de la classe main

---
+++ src/com/napster/calls/Main.java
@@ -0,0 +1,12 @@
+package com.napster.calls;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// our programm is a console programm so we are going to use the main class to perform our tests
+        
+        System.out.println(" test de la fonction main");
+
+    }
+}
