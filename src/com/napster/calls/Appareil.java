6e881e4f9c2644baf39eef4b7c463910bcae92e9
napster <napsterpaulin@gmail.com>
2019-08-28 02:44:41 +0200

    implementation des methodes de l'interface dans la classe appareil

--- src/com/napster/calls/Appareil.java
+++ src/com/napster/calls/Appareil.java
@@ -13,10 +13,25 @@ private Map<Integer,Appel> appels = new HashMap<Integer, Appel>();
     @Override
     public void enregistrer(Contacts c) {
 
+
+
     }
 
     @Override
     public void enregistrer(Appel a, String numtel) {
+        Contacts c1 = null;
+
+        for (Contacts c: contact)
+        {
+            if (c.getNumeroTel().equals(numtel))
+            {
+                c1=c;
+                break;
+            }
+            a.setContact(c1);
+            appels.put(a.getNumero(),a);
+        }
+
 
     }
 
